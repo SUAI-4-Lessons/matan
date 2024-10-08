b74d5a9ef1cfa3d6aa50af12dedd17f4452cb96f
pank-su <frostkslo1@yandex.ru>
2024-10-08 13:00:32 +0300

    feat: add css styles

--- publish.el
+++ publish.el
@@ -12,6 +12,24 @@
                 "ico" "cur" "css" "js" "woff" "html" "pdf"))
   "File types that are published as static files.")
 
+(setq org-html-head "
+<style type=\"text/css\">
+ @import url('https://unpkg.com/@catppuccin/palette/css/catppuccin.css');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+  body{
+      background: var(--ctp-latte-base);
+  }
+
+  *{
+      font-family: \"Roboto\", serif;
+      color: var(--ctp-latte-text);
+  }
+</style>
+")
+
+(setq org-html-postamble nil)
+
 
 
 (setq org-publish-project-alist
